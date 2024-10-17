4f8cf1b8cb1be77a689e13d8d76bad71068c8e41
danielW100 <s-wangdanie@bsd405.org>
2024-10-16 17:30:27 -0700

    Create HelloWorld.java

---
+++ asciiart/HelloWorld.java
@@ -0,0 +1,10 @@
+public class HelloQuestion{
+  public static void main(String[]args){
+    System.out.printLn( _          _ _                            _     _ );
+    System.out.println(| |        | | |                          | |   | |);
+    System.out.println(| |__   ___| | | ___   __      _____  _ __| | __| |);
+    System.out.println(| '_ \ / _ \ | |/ _ \  \ \ /\ / / _ \| '__| |/ _` |);
+    System.out.println(| | | |  __/ | | (_) |  \ V  V / (_) | |  | | (_| |);
+    System.out.println(|_| |_|\___|_|_|\___/    \_/\_/ \___/|_|  |_|\__,_|);
+  }
+}
