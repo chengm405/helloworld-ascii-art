5432340684f28ce636a76718914ba28a97eca1b8
danielW100 <s-wangdanie@bsd405.org>
2024-10-21 17:31:10 -0700

    Create helloWorld.cs

---
+++ asciiart/helloWorld.cs
@@ -0,0 +1,10 @@
+using System;
+public class helloWorld{
+  public static void main(string[] args){
+    Console.WriteLine("    __  __     ____         _       __           __    __");
+    Console.WriteLine("   / / / /__  / / /___     | |     / /___  _____/ /___/ /");
+    Console.WriteLine("  / /_/ / _ \/ / / __ \    | | /| / / __ \/ ___/ / __  / ");
+    Console.WriteLine(" / __  /  __/ / / /_/ /    | |/ |/ / /_/ / /  / / /_/ /  ");
+    Console.WriteLine("/_/ /_/\___/_/_/\____/     |__/|__/\____/_/  /_/\__,_/   ");                
+  }
+}
